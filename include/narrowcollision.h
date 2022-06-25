e99e6bf395b52a8dd1986cfe032bd298f9da93b3
ioplkm <ioplkm352@gmail.com>
2022-06-26 02:27:35 +0500

    save

--- include/narrowcollision.h
+++ include/narrowcollision.h
@@ -14,14 +14,14 @@ typedef struct {
   Rigidbody *pRB;
   Matrix34 offset;
   double r;
-} Sphere;
+} CollisionSphere;
 
 typedef struct {
   Rigidbody *pRB;
   Matrix34 offset;
   Vector normal;
   double planeOffset;
-} Plane;
+} CollisionPlane;
 
 typedef struct {
   Vector p;
@@ -32,35 +32,35 @@ typedef struct {
 Collision collisions[999];
 int collisionC;
 
-void SphereSphereCollision(Sphere *pS1, Sphere *pS2) {
-  Vector s1p = pS1->pRB->p;
-  Vector s2p = pS2->pRB->p;
+void SphereSphereCollision(CollisionSphere *pCS1, CollisionSphere *pCS2) {
+  Vector s1p = pCS1->pRB->p;
+  Vector s2p = pCS2->pRB->p;
   Vector midline = vSub(s1p, s2p);
   double size = vLength(midline);
 
   collisions[collisionC].p = vAdd(s1p, vMult(midline, 0.5));
   collisions[collisionC].normal = vMult(midline, 1/size);
-  collisions[collisionC].penetration = pS1->r + pS2->r - size;
+  collisions[collisionC].penetration = pCS1->r + pCS2->r - size;
   collisionC++;
 }
 
-void SphereHalfSpaceCollision(Sphere *pS, Plane *pP) {
-  Vector sp = pS->pRB->p;
-  double distance = scalarProd(pP->normal, sp) - pS->r - pP->planeOffset;
+void SphereHalfSpaceCollision(CollisionSphere *pCS, CollisionPlane *pCP) {
+  Vector sp = pCS->pRB->p;
+  double distance = scalarProd(pCP->normal, sp) - pCS->r - pCP->planeOffset;
   
-  collisions[collisionC].p = vSub(sp, vMult(pP->normal, distance + pS->r));
-  collisions[collisionC].normal = pP->normal;
+  collisions[collisionC].p = vSub(sp, vMult(pCP->normal, distance + pCS->r));
+  collisions[collisionC].normal = pCP->normal;
   collisions[collisionC].penetration = -distance;
   collisionC++;
 }
 
-void SpherePlaneCollision(Sphere *pS, Plane *pP) {
-  Vector sp = pS->pRB->p;
-  double distance = scalarProd(pP->normal, sp) - pP->planeOffset;
+void SpherePlaneCollision(CollisionSphere *pCS, CollisionPlane *pCP) {
+  Vector sp = pCS->pRB->p;
+  double distance = scalarProd(pCP->normal, sp) - pCP->planeOffset;
 
-  collisions[collisionC].p = vSub(sp, vMult(pP->normal, distance + pS->r));
-  collisions[collisionC].normal = distance < 0 ? vMult(pP->normal, -1) : pP->normal;
-  collisions[collisionC].penetration = distance < 0 ? distance + pS->r : -distance + pS->r;
+  collisions[collisionC].p = vSub(sp, vMult(pCP->normal, distance + pCS->r));
+  collisions[collisionC].normal = distance < 0 ? vMult(pCP->normal, -1) : pCP->normal;
+  collisions[collisionC].penetration = distance < 0 ? distance + pCS->r : -distance + pCS->r;
   collisionC++;
 }
 
