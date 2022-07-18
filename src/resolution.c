1a300e2eb53d3f72597ac95c86981cf120a020ad
ioplkm <ioplkm352@gmail.com>
2022-07-19 04:47:44 +0500

    collision basis

--- src/resolution.c
+++ src/resolution.c
@@ -3,5 +3,8 @@
 #include "../inc/narrowcollision.h"
 
 void resolveCollision(Collision *pC) {
-  Vector localX = pC->normal;
+  Vector lX = vNorm(pC->normal);
+  Vector lY = fabs(lX.x) > fabs(lX.y) ? (Vector){0, 1, 0} : (Vector){1, 0, 0};
+  Vector lZ = vNorm(vectorProd(lX, lY)); 
+  lY = vNorm(vectorProd(lX, lZ));
 }
